bb0e13c5a34a56ffdf5a4e0a4bc3ba24ff61a207
Zinc Glaze <zekegraves@gmail.com>
2018-10-07 16:30:36 -0400

    added 'trivia cards' object, 'next card' function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,8 @@ var wrong;
 var intervalId;
 //whether timer is running or not
 var clockRunning = false;
+//stores question count
+var cardNum = 0;
 
 //FUNCTIONS
 
@@ -21,12 +23,55 @@ function restart() {
     $("#box2").empty();
     var startButton = $("<button>");
     startButton.text("Start!");
-    startButton.addClass("startButton");
+    startButton.addClass("start-button");
     $("#box1").append(startButton);
 }
 
+function nextCard() {
+    $("#timer").text(30);
+    timer.start();
+    $("#box1").text(triviaDeck.cards[cardNum].question);
+    var multipleChoice = $("<ul>");
+    for (i = 0; i < 4; i++) {
+        var choice = $("<li>");
+        choice.text(triviaDeck.cards[cardNum].choices[i]);
+        choice.attr("data-choice", i);
+        choice.addClass("answer-button");
+        multipleChoice.append(choice);
+    }
+    $("#box2").append(multipleChoice);
+    cardNum++;
+}
 
-//TIMER OBJECT
+function pause() {
+
+}
+
+
+//TRIVIA DECK OBJECT
+var triviaDeck = {
+    //Cards
+    cards: [{
+        question: "Which singer made a comeback as a 'Private Dancer' in 1984?",
+        choices: ["Diana Ross", "Steve Winwood", "Stevie Wonder", "Tina Turner"],
+        image:  "",
+        validAnswer: 3
+        }, {
+        question: "Which artist released a tribute to autoerotic stimulation titled 'She Bop' in 1984?",
+        choices: ["Belinda Carlisle", "Cindy Lauper", "Madonna", "Prince"],
+        image:  "",
+        validAnswer: 1
+        }],
+    //Shuffle deck    
+    shuffle: function() {
+        cards.sort(function() {
+            return 0.5 - Math.random();
+        });
+    }
+}
+
+
+//QUESTION TIMER OBJECT
 var timer = {
 
     time: 30,
@@ -52,6 +97,7 @@ var timer = {
         //decrement time by 1
         timer.time--;
         $("#timer").text(timer.time);
+        
     }
 }
 
@@ -60,4 +106,8 @@ var timer = {
 
 window.onload = function() {
     restart();
-}
+    $(".start-button").on("click", nextCard);
+
+ 
+};
+
